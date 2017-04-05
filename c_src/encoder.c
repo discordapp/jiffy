c3f2c2f2c9933cc38d97ea3ce1573a4cade48889
Jake <jh@discordapp.com>
2017-04-05 12:30:05 -0700

    add support for incoding int64 as string

--- c_src/encoder.c
+++ c_src/encoder.c
@@ -35,6 +35,7 @@ typedef struct {
     int             pretty;
     int             use_nil;
     int             escape_forward_slashes;
+    int             bigint_as_string;
 
     int             shiftcnt;
     int             count;
@@ -79,6 +80,7 @@ enc_new(ErlNifEnv* env)
     e->pretty = 0;
     e->use_nil = 0;
     e->escape_forward_slashes = 0;
+    e->bigint_as_string = 0;
     e->shiftcnt = 0;
     e->count = 0;
 
@@ -403,8 +405,13 @@ enc_string(Encoder* e, ERL_NIF_TERM val)
 static inline int
 enc_long(Encoder* e, ErlNifSInt64 val)
 {
-    if(!enc_ensure(e, 32)) {
+    if(!enc_ensure(e, e->bigint_as_string ? 34 : 32)) {
         return 0;
+    }
+
+    if (e->bigint_as_string) {
+        e->p[e->i] = '"';
+        e->i++;
     }
 
 #if (defined(__WIN32__) || defined(_WIN32) || defined(_WIN32_))
@@ -416,6 +423,13 @@ enc_long(Encoder* e, ErlNifSInt64 val)
 #endif
 
     e->i += strlen(&(e->p[e->i]));
+
+    if (e->bigint_as_string) {
+        e->p[e->i] = '"';
+        e->i++;
+        e->p[e->i] = 0;
+    }
+
     e->count++;
 
     return 1;
@@ -610,6 +624,8 @@ encode_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
             e->escape_forward_slashes = 1;
         } else if(enif_compare(val, e->atoms->atom_use_nil) == 0) {
             e->use_nil = 1;
+        } else if(enif_compare(val, e->atoms->atom_bigint_as_string) == 0) {
+            e->bigint_as_string = 1;
         } else if(enif_compare(val, e->atoms->atom_force_utf8) == 0) {
             // Ignore, handled in Erlang
         } else if(get_bytes_per_iter(env, val, &(e->bytes_per_red))) {
